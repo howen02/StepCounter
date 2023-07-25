52320a39462f6f3d9b6b8617cb4c0955da0a77e5
Howenyap <e0985925@u.nus.edu>
2023-07-26 00:47:38 +0800

    extract Value to a separate component

---
+++ StepCounter/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Value from './src/components/Value';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Howen!</Text>
+
+      <View style={styles.values}>
+        <Value label="Steps" value="1219" />
+        <Value label="Distance" value="0.75km" />
+        <Value label="Flights Climbed" value="100" />
+      </View>
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    padding: 12, 
+  },
+  values: {
+    flexDirection: 'row', 
+    gap: 25,
+    flexWrap: 'wrap'
+  }
+});
